cff15a0c557c976e79564a88866f4b9a58531b94
IanConsulting <martoxdlol@gmail.com>
2023-04-29 01:13:26 -0300

    Show appbar title on mobile too

--- src/lib/components/AppBar.tsx
+++ src/lib/components/AppBar.tsx
@@ -165,7 +165,7 @@ export default function SearchAppBar() {
                             variant="h6"
                             noWrap
                             component="div"
-                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                            sx={{ flexGrow: 1 }}
                         >
                             Comunicaciones
                         </Typography>
