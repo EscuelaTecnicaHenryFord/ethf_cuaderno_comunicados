d1510d77bc9313b930384b6200c3b929f5349600
Tomás Cichero <martoxdlol@gmail.com>
2024-04-05 15:20:05 -0300

    fix json formatting

--- src/pages/api/settings/[file].ts
+++ src/pages/api/settings/[file].ts
@@ -8,5 +8,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({ error: "Invalid filename" })
     }
 
-    res.json(await settings.getFile(file))
+    res.json(JSON.parse(await settings.getFile(file)))
 }
